c690a071144500056424e92a839841ff6f3cf621
Jozef Virag <jozef.virag10@gmail.com>
2025-09-19 12:50:53 +0200

    init

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+import tailwind from '@astrojs/tailwind';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#0057FF',
+                secondary: '#00C2FF'
+            }
+        },
+    },
+    plugins: [],
+    integrations: [tailwind()]
+}
